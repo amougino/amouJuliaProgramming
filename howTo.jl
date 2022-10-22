53f829fb12054fe4b9f024f2e55dca799e13b603
A Mouginot <mouginot.arthur@gmail.com>
2022-10-22 18:04:38 +0200

    adding all known julia files

---
+++ howTo.jl
@@ -0,0 +1,14 @@
+#= This is how you write a comment =#
+
+#= This is my personalized guide into Julia =#
+
+#= To execute a file in Julia, go to the Julia REPL (Read–eval–print loop) and type include("path") =#
+
+#= Importing =#
+
+b = "done"
+for i in 0:1:10000000000
+end
+println(b)
+b = 2
+println(b)
