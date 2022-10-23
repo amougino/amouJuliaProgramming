fd3bb23ecc4441a554869f7c953901d3b3446a74
A Mouginot <mouginot.arthur@gmail.com>
2022-10-23 14:38:00 +0200

    added mastermind.jl

--- gtkHowTo.jl
+++ gtkHowTo.jl
@@ -1,26 +1,23 @@
 using Gtk
 
-width = 1000
-height = 1200
+width = 1600
+height = 800
 win = GtkWindow("Don't press 11",width,height)
-spacing = 10
-horizontalSpacing = (width - 0) / (4 * spacing)
-verticalSpacing = (height - 0) / (12 * spacing)
 sideSpacing = 6
 grid = GtkGrid()
 
 #Positionnement des boutons dans la grille 5x6
 nb=1
 for line in 0:11, row in 0:3
-    grid[row,line] = GtkButton("$nb")
+    grid[row,line] = GtkLabel("$nb")
     println("($row,$line) = $nb")
     global nb += 1
 end
 
 set_gtk_property!(grid, :column_homogeneous, true)
 set_gtk_property!(grid, :row_homogeneous, true)
-set_gtk_property!(grid, :column_spacing, horizontalSpacing)
-set_gtk_property!(grid, :row_spacing, verticalSpacing)
+set_gtk_property!(grid, :column_spacing, sideSpacing)
+set_gtk_property!(grid, :row_spacing, sideSpacing)
 set_gtk_property!(grid, :margin_top, sideSpacing)
 set_gtk_property!(grid, :margin_bottom, sideSpacing)
 set_gtk_property!(grid, :margin_left, sideSpacing)
@@ -30,7 +27,10 @@ function on_button_clicked(w)
     println("The button has been clicked")
     Gtk.destroy(win)
 end
-
+label = GtkLabel("My text")
+GAccessor.markup(label,"""<b>My bold text</b>\n
+                          <a href=\"http://www.gtk.org\"
+                          title=\"Our website\">GTK+ website</a>""")
 signal_connect(on_button_clicked, grid[2,2], "clicked")
 push!(win, grid)
 showall(win)
