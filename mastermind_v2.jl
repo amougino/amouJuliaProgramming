cf6e8778bc937858e8bd1cd50be26b8209a4db4e
A Mouginot <mouginot.arthur@gmail.com>
2022-10-25 15:16:33 +0200

    many major changes, including checking algorithm (test6.jl)

--- mastermind_v2.jl
+++ mastermind_v2.jl
@@ -8,17 +8,29 @@ winHeight = winSize*250
 margin = 10
 gen = 0
 
-global col = 0
-global row = 0
+col = 0
+row = 0
 
 buttonList = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Brown", "Pink", "Delete", "Enter"]
-global colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Brown", "Pink"]
+colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Brown", "Pink"]
+
+default = [0.75,0.75,0.75]
+
+red = [1,0,0]
+orange = [1,0.5,0]
+yellow = [1,1,0.1]
+green = [0,1,0]
+blue = [0,0,1]
+purple = [0.6,0.12,0.94]
+brown = [0.6,0.3,0]
+pink = [1,0.75,0.8]
+
+currentCode = []
 
 function generateCode()
     code = []
     while size(code)[1] <= 3
         obj = Random.rand(colors, 1)
-        filter!(e -> e ≠ obj, colors)
         push!(code, obj)
     end
     return(code)
@@ -26,25 +38,102 @@ end
 
 function onButtonClicked(button)
     println("$( get_gtk_property(button, :label, String) ) has been clicked")
-    c.draw = drawC
-    draw(c)
+    global col
+    global row
+    global colors
+    global currentCode
+
+    buttonName = get_gtk_property(button, :label, String)
+    if buttonName in colors
+        if col < 4
+            c.draw = eval(Meta.parse("drawC$buttonName"))
+            draw(c)
+            col += 1
+            push!(currentCode, buttonName)
+        end
+    elseif buttonName == "Delete"
+        if col >= 1
+            col -= 1
+            c.draw = deleteC
+            draw(c)
+            deleteat!(currentCode, size(currentCode)[1])
+        end
+    elseif buttonName == "Enter"
+        println(col)
+        if col == 4
+
+        end
+        println(currentCode)
+    end
 end
 
-function drawC(widget)
-    if col < 4
-        ctx = getgc(widget)
-        rectangle(ctx, ( ( winWidth / 3.5 ) + ( ( winWidth / 6 ) * col ) ), ( ( winHeight / 15 ) + ( ( winHeight / 14 ) * row ) ), ( winWidth / 20 ), ( ( winHeight * ( 4 / 5 ) ) / 20 ) )
-        set_source_rgb(ctx, 1, 0.75, 0.75)
-        fill(ctx)
-        global col
-        global row
-        col += 1
-        if col == 4
-            col = 0
-            row += 1
+function  drawCColor(widget, array)
+    ctx = getgc(widget)
+    rectangle(ctx, ( ( ( winWidth / 3.5 ) + ( ( winWidth / 6 ) * col ) ) + 2 ), ( ( ( winHeight / 15 ) + ( ( winHeight / 14 ) * row ) ) + 2 ), ( ( winWidth / 20 ) - 4 ), ( ( ( winHeight * ( 4 / 5 ) ) / 20 )  - 4 ) )
+    set_source_rgb(ctx, array[1], array[2], array[3])
+    fill(ctx)
+end
+
+function drawCRed(widget)
+    drawCColor(widget, red)
+end
+
+function drawCOrange(widget)
+    drawCColor(widget, orange)
+end
+
+function drawCYellow(widget)
+    drawCColor(widget, yellow)
+end
+
+function drawCGreen(widget)
+    drawCColor(widget, green)
+end
+
+function drawCBlue(widget)
+    drawCColor(widget, blue)
+end
+
+function drawCPurple(widget)
+    drawCColor(widget, purple)
+end
+
+function drawCBrown(widget)
+    drawCColor(widget, brown)
+end
+
+function drawCPink(widget)
+    drawCColor(widget, pink)
+end
+
+function deleteC(widget)
+    ctx = getgc(widget)
+    rectangle(ctx, ( ( winWidth / 3.5 ) + ( ( winWidth / 6 ) * col ) ), ( ( winHeight / 15 ) + ( ( winHeight / 14 ) * row ) ), ( winWidth / 20 ), ( ( winHeight * ( 4 / 5 ) ) / 20 ) )
+    set_source_rgb(ctx, default[1], default[2], default[3])
+    fill(ctx)
+end
+
+function checkCode(code,answer)
+    wrongPlacement = 0
+    rightPlacement = 0
+    for i in 1:4
+        if answer[i] == code[i]
+            answer[i] = 0
+            code[i] = 0
+            rightPlacement += 1
+        end
+        
+    end
+    for i in 1:4, j in 1:4
+        if answer[i] != 0
+            if answer[i] == code[j]
+                answer[i] = 0
+                code[j] = 0
+                wrongPlacement += 1
+            end
         end
     end
-    println(col, row)
+    return([wrongPlacement,rightPlacement])
 end
 
 win = GtkWindow("Mastermind v1.1", winWidth, winHeight)
@@ -59,7 +148,7 @@ bh = GtkBox(:h)
     for line in 0:3, row in 0:11 
         rectangle(ctx, ( ( winWidth / 3.5 ) + ( ( winWidth / 6 ) * line ) ), ( ( winHeight / 15 ) + ( ( winHeight / 14 ) * row ) ), ( winWidth / 20 ), ( ( winHeight * ( 4 / 5 ) ) / 20 ) )
     end
-    set_source_rgb(ctx, 0.75, 0.75, 0.75)
+    set_source_rgb(ctx, default[1], default[2], default[3])
     fill(ctx)
 end
 
